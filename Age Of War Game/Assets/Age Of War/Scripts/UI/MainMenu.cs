178bc3e1d8597718a8c5d3848be6007b6d1729a9
KD-Kevin <37353878+KD-Kevin@users.noreply.github.com>
2022-02-26 14:28:16 -0700

    Updated Main Menu Scene

--- Age Of War Game/Assets/Age Of War/Scripts/UI/MainMenu.cs
+++ Age Of War Game/Assets/Age Of War/Scripts/UI/MainMenu.cs
@@ -23,4 +23,29 @@ public class MainMenu : MonoBehaviour
             Application.Quit();
         }
     }
+
+    public void OpenVsCpuPage()
+    {
+
+    }
+
+    public void OpenRankedPage()
+    {
+
+    }
+
+    public void OpenCustomGamePage()
+    {
+
+    }
+
+    public void JoinQuickplayQueue()
+    {
+
+    }
+
+    public void OpenCampaignPage()
+    {
+
+    }
 }
