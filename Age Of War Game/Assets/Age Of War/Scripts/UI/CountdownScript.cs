4d9ca8e471673657f4284b8c4043be63cd22a26f
KD-Kevin <37353878+KD-Kevin@users.noreply.github.com>
2022-04-20 15:31:53 -0600

    Added Countdown

---
+++ Age Of War Game/Assets/Age Of War/Scripts/UI/CountdownScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownScript : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TextMeshProUGUI CountdownText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LockstepManager.Instance.SecondsTillReconnect == -1)
+        {
+            gameObject.SetActive(false);
+        }
+
+        CountdownText.text = $"{LockstepManager.Instance.SecondsTillReconnect}";
+    }
+}
